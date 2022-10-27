8e6d74f76b7f24c470d9d2c543844ae9051a745b
Fay <makkarennu14@gmail.com>
2022-10-27 13:15:54 +0700

    remedial ujian lisan

---
+++ remedial_fay.js
@@ -0,0 +1,28 @@
+var a = 10;
+var b = 20;
+
+for ( i = 0; i<b; i++){
+    console.log("nilai a lebih besar")
+    //console.log("nilai b lebih besar");
+}
+
+/*for(a>b){
+    console.log("nilai a lebih besar");
+    console.log("nilai b lebih besar");
+}*/
+
+var a = 10;
+var b = 20;
+console.log(a != b);
+
+while (18 < 20) {
+console.log("while")
+}
+
+var z = 25;
+
+do {
+console.log("perulangan akan dimulai")
+} while (z < 20) {
+console.log(z)
+}
